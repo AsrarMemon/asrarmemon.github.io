47b077d5980d2b35daee37ffea69d9fcad22a816
asrar.memon <asrar.memon0001@gmail.com>
2020-10-03 21:44:34 +0530

    Updates

---
+++ index.js
@@ -0,0 +1,26 @@
+let d = new Date();
+console.log(document.body)
+
+let data = []
+// document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+
+const createPdf = () => {
+    const spaceInfo = {}
+    spaceInfo.address = document.getElementById('address').value
+    spaceInfo.description = document.getElementById('desc').value
+    spaceInfo.amount = document.getElementById('amount').value
+    var newWindow = window.open('./index.html');
+    data.push(spaceInfo)
+    localStorage.setItem('data_to_print', JSON.stringify(data))
+}
+
+const addNewSpace = () => {
+    const spaceInfo = {}
+    spaceInfo.address = document.getElementById('address')
+    spaceInfo.description = document.getElementById('desc')
+    spaceInfo.amount = document.getElementById('amount')
+    data.push(spaceInfo)
+    document.getElementById('address').innerHTML = ''
+    document.getElementById('desc').innerHTML = ''
+    document.getElementById('amount').innerHTML = ''
+}
